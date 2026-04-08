4b12716129c911027073474766d635dc0f834e9e
sravani <2500032418@kluniversity.in>
2026-04-08 12:43:50 +0530

    adding files

---
+++ CaseStudy_2.py
@@ -0,0 +1,6 @@
+#To check whether number is even or odd
+num=6
+if num%2==0:
+    print("number is even")
+else:
+    print("number is odd")
---
+++ CaseStudy_3.py
@@ -0,0 +1,12 @@
+"""Arithmetic operators used in robot navigation"""
+
+#initial position
+y=0
+print("Start position:",y)
+#Move Up
+y=y+1
+print("End position:",y)
+#Move Down
+y=y-1
+print("Start position:",y)
+print("Final position:",y)
