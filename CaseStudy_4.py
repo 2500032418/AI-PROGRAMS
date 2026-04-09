de958e5d917b09d5695d46dcf8e3bff62a9cc101
sravani <2500032418@kluniversity.in>
2026-04-09 12:23:49 +0530

    modified

---
+++ CaseStudy_4.py
@@ -0,0 +1,7 @@
+"""comparison marks for two students"""
+a=45
+b=78
+if a>b:
+    print("a is greater ")
+else:
+    print("b is greater ")
---
+++ CaseStudy_5.py
@@ -0,0 +1,6 @@
+"""temperature checking"""
+temp=30
+if temp>30:
+    print("High temperature")
+else:
+    print("Low temperature")
